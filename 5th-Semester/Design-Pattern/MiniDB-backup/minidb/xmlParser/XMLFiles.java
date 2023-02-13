ee248e2b09a9981bcc9f44fca89a4298aa80736d
abj-paul <bsse1201@iit.du.ac.bd>
2023-02-13 19:32:57 +0600

    Unsolicited changes

--- 5th-Semester/Design-Pattern/MiniDB-backup/minidb/xmlParser/XMLFiles.java
+++ 5th-Semester/Design-Pattern/MiniDB-backup/minidb/xmlParser/XMLFiles.java
@@ -14,43 +14,6 @@ public abstract class XMLFiles {
     protected File xmlFile;
     protected Document doc;
 
-    private void createNewFile(String path) throws IOException, SAXException, ParserConfigurationException{
-	this.xmlFile = Factory.createFileObject(path);
-	boolean NoFileFound = xmlFile.createNewFile();
-	load(NoFileFound);
-    }
-
-    public XMLFiles(String path) {
-        try {
-		Factory.createDirectoryForPath(path);
-	    	createNewFile(path);
-	    	
-                    
-	} catch (ParserConfigurationException | SAXException | IOException err) {
-            System.out.println(err);
-            err.printStackTrace();
-        }
-    }
-
-
-    private void load(boolean NoFile) throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-        if (NoFile) {
-            doc = docBuilder.newDocument();
-            createFile(); 
-        } else {
-            doc = docBuilder.parse(xmlFile);
-        }
-    }
-
-    abstract void createFile();
-    
-    /**
-     * Call this method to update the XML file.
-     */
-
-
     private void transformDOMToStream(Transformer transformer) throws TransformerException{
             DOMSource source = new DOMSource(this.doc);
             StreamResult result = new StreamResult(this.xmlFile);
