79641c7c590db78dee26cb4936763fffec8d2a5f
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-04-27 22:04:14 +0600

    final prep

--- ecommerce/src/app/profile/profile.component.ts
+++ ecommerce/src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccessControlService } from '../services/access-control.service';
 import { Router } from '@angular/router';
 
@@ -7,8 +7,22 @@ import { Router } from '@angular/router';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+
+  username:string = "";
+  age:number = 0;
+  email: string = "";
+  role: string = "";
+
+  ngOnInit(): void {
+    const user = this.accessControlService.getLoggedInUser();
+    this.username = user.username;
+    this.age = user.age;
+    this.email = user.email;
+    this.role = user.role;
+  }
   constructor(private accessControlService: AccessControlService, private router: Router){}
+
   logout(){
     this.accessControlService.logout();
     this.router.navigate(["login"]);
